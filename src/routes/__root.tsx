fb06e6da2968743dad28dd73fb2ac527ccf8992e
Salvatore Gentile <884093+SalGnt@users.noreply.github.com>
2026-08-01 12:53:08 +0200

    chore: switch to Oxc

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,13 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const RootLayout = () => (
   <>
     <div className="p-2 flex gap-2">
       <Link to="/" className="[&.active]:font-bold">
         Home
-      </Link>{' '}
+      </Link>{" "}
       <Link to="/about" className="[&.active]:font-bold">
         About
       </Link>
@@ -17,6 +17,6 @@ const RootLayout = () => (
     <ReactQueryDevtools initialIsOpen={false} />
     <TanStackRouterDevtools />
   </>
-)
+);
 
-export const Route = createRootRoute({ component: RootLayout })
+export const Route = createRootRoute({ component: RootLayout });
